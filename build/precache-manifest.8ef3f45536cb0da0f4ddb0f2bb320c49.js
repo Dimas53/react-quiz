83f793cc8bf653e287d59d5a9d6fe6d463b36d63
Dmitriis53 <dima.savcenco@gmail.com>
2020-01-11 23:09:29 +0200

    commit finish last

---
+++ build/precache-manifest.8ef3f45536cb0da0f4ddb0f2bb320c49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b1540481974c897ce43bdda2cf1722",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdc75b4393ba8918c545",
+    "url": "/static/css/main.7409ec5d.chunk.css"
+  },
+  {
+    "revision": "ec37ea2a4af1f587d1dd",
+    "url": "/static/js/2.b300c64d.chunk.js"
+  },
+  {
+    "revision": "c7779491f48b00d5d04d8df4a1abb7e7",
+    "url": "/static/js/2.b300c64d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fdc75b4393ba8918c545",
+    "url": "/static/js/main.0b73ac0e.chunk.js"
+  },
+  {
+    "revision": "aea0ec5890e8dc094742",
+    "url": "/static/js/runtime-main.4ae3a700.js"
+  }
+]);
